0f0b4253228d47965aefc4fa163ef6e9c4bbe442
Xiaochuang Han <xc@gatech.edu>
2018-04-21 18:42:46 -0400

    hello

--- gtstore/client.py
+++ gtstore/client.py
@@ -5,23 +5,8 @@ import pickle
 from util import *
 from constant import *
 from server import *
+from gtstore_api import *
 
 if __name__ == "__main__":
 
-    ip_addr = 'localhost'
-    port = 4210
-
-    # create an INET, STREAMing socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # now connect to the server
-    client_socket.connect((ip_addr, port))
-    
-    # send hand-shake msg
-    msg = MSG_HELLO.encode()
-    send_msg(client_socket, msg)
-
-    # recv node list
-    pickled_node_list = recv_msg(client_socket)
-
-    node_list = pickled_node_list
-    print(pickle.loads(pickled_node_list))
+    conn = DBConnection('localhost', 4210)
--- gtstore/gtstore_api.py
+++ gtstore/gtstore_api.py
@@ -3,14 +3,34 @@ GT Store Library API
 '''
 
 import socket
+import sys
+import pickle
+
+from util import *
+from constant import *
+from server import *
+import hashlib
 
 class DBConnection:
     def __init__(self, master_ip_addr, master_port):
 
+        self.master_ip_addr = master_ip_addr
+        self.master_port = master_port
+
         # create an INET, STREAMing socket
-        conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # now connect to the server
-        conn_socket.connect((master_ip_addr, master_port))
+        client_socket.connect((master_ip_addr, master_port))
+
+        # send hand-shake msg
+        msg = MSG_HELLO.encode()
+        send_msg(client_socket, msg)
+
+        # recv node list
+        pickled_node_info_list = recv_msg(client_socket)
+
+        self.node_info_list = pickle.loads(pickled_node_info_list)
+        print(self.node_info_list)
 
     def put(self, key, value):
         pass
